72933e7440361b9c7269eed0785ac70b6f10b8bc
sagar-tan <tanwarsagarrobotics2274@gmail.com>
2024-12-22 20:42:17 +0530

    2_Subset_TOP Down Completed

--- 2_Subset.cpp
+++ 2_Subset.cpp
@@ -1,9 +1,9 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-bool fxn(vector<vector<int>>& dp, int index, vector<int>& arr,int n,int target){//Top Down
+bool canPart_TD(vector<vector<int>>& dp, int index, vector<int>& arr,int n,int target){//Top Down
     if (target==0){
-        return 0;
+        return 1;
     }
     if (target<0){
         return false;
@@ -11,8 +11,8 @@ bool fxn(vector<vector<int>>& dp, int index, vector<int>& arr,int n,int target){
     if (index>=n){
         return false;
     }
-    bool include = fxn(dp, index +1, arr, n, target-arr[index]);
-    bool exclude = fxn(dp, index +1, arr, n, target);
+    bool include = canPart_TD(dp, index +1, arr, n, target-arr[index]);
+    bool exclude = canPart_TD(dp, index +1, arr, n, target);
     return include||exclude;
 
 
@@ -27,17 +27,21 @@ int main(){
     vector<int> arr = {1,5,11,5};
     int n = arr.size();
     int totalsum = 0;
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<=n-1;i++){
         totalsum+=arr[i];
     }
-    if(totalsum & 1) return 0;
+    if(totalsum & 1){
+        cout<<"Unable to Divide";
+      return 0;
+    }
     int target = totalsum/2;
     int index = 0;
-
     vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-    
-    bool j = fxn(dp, index, arr, n, target);
-    cout << j;
+    bool j = canPart_TD(dp, index, arr, n, target);
+    if(j){
+        cout<<"Can be Divided";
+    }
+  return 0;
     
 
 }
