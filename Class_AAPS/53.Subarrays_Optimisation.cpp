ee6b67978868146a5e6f1b67e2b50c944914642f
sagar-tan <tanwarsagarrobotics2274@gmail.com>
2025-03-06 11:49:10 +0530

    Optimised Approach for Q. 53

---
+++ Class_AAPS/53.Subarrays_Optimisation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        int maxSubArray_Unoptimised(vector<int>& nums) {
+            int n = nums.size();
+            int prevMax=nums[0];
+            int maxSum = nums[0];
+            for(int i=1; i < n; i++){
+                int curr_sum = max(prevMax + nums[i], nums[i]);
+                maxSum = max(curr_sum, maxSum);
+                prevMax = curr_sum;
+            }
+            return maxSum;
+        }
+    };
+int main(){
+
+}
