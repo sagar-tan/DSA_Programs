473fa4816a954b6512075a7b1db82f2ae40ac875
sagar-tan <tanwarsagarroboticss2274@gmail.com>
2025-06-22 11:10:27 +0530

    Remove_Digits solution accepted, writing solution for ferris wheel

--- CSES_Programs/Remove_digits.cpp
+++ CSES_Programs/Remove_digits.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int temp = n;
     int steps=0;
-    int maxDigit=0;
+
     while(n>0){
+        int temp = n;
+        int maxDigit=0;
         while(temp>0){
             maxDigit = max(maxDigit, temp%10);
             temp/=10;
