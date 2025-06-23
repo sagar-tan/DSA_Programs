1b6ee1054c9917b6b9357d1b1a9e2dbd97cedb96
sagar-tan <tanwarsagarroboticss2274@gmail.com>
2025-06-23 10:01:37 +0530

    update

---
+++ CSES_Programs/Ferris_wheel.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> weight(n);
+    for (int i = 0; i < n; ++i)
+        cin >> weight[i];
+    sort(weight.begin(), weight.end());
+    int i = 0, j = n - 1, gondolas = 0;
+
+    while (i <= j) {
+        if (weight[i] + weight[j] <= x) {
+            i++;
+            j--;
+        } else {
+            j--;
+        }
+        gondolas++;
+    }
+
+    cout << gondolas << endl;
+    return 0;
+}
