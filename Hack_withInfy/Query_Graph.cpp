950eeebaa08c83b792b1a5bb43cbaff79fcec7d8
sagar-tan <tanwarsagarroboticss2274@gmail.com>
2025-07-01 15:46:57 +0530

    HackWiftyINFY-Hard Question

--- Hack_withInfy/Query_Graph.cpp
+++ Hack_withInfy/Query_Graph.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 const int MAXN = 100005;
 int parent[MAXN], rank1[MAXN];
+map<int, set<int>> componentMap;
+map<int, int> componentBeauty;
 
 int find(int x) {
     if (parent[x] != x)
@@ -13,7 +15,6 @@ int find(int x) {
     return parent[x];
 }
 
-// Union by Rank, returns new root
 int union_sets(int x, int y) {
     if (rank1[x] < rank1[y]) {
         parent[x] = y;
@@ -26,4 +27,55 @@ int union_sets(int x, int y) {
         rank1[y]++;
         return y;
     }
+}
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    for(int i = 1; i<=n; i++){
+        parent[i] = i;
+        rank1[i] = 0;
+        set<int> s;
+        s.insert(i);
+        componentMap[i] = s;
+        componentBeauty[i] = 1;
+    }
+    long long totalBeauty = 0;
+    while(q--){
+        int type, u, v;
+        cin >> type >> u >> v;
+        if(type == 1){
+            int ru = find(u);
+            int rv = find(v);
+            if(ru != rv){
+                int newRoot = union_sets(ru, rv);
+                int oldRoot = (newRoot == ru) ? rv:ru;
+                set<int> & setNew = componentMap[newRoot];
+                set<int> & setOld = componentMap[oldRoot];
+                int beauty = componentBeauty[newRoot];
+                for(int node : setOld){
+                    bool hasLeft = setNew.count(node - 1);
+                    bool hasRight = setNew.count(node + 1);
+                    if(hasLeft && hasRight){
+                        beauty--;
+                    }
+                    else if(!hasLeft && !hasRight){
+                        beauty++;
+                    }
+                    setNew.insert(node);
+                }
+                componentMap[newRoot] = setNew;
+                componentMap.erase(oldRoot);
+                componentBeauty[newRoot] = beauty;
+                componentBeauty.erase(oldRoot);
+            }
+        }
+        else if (type == 2) {
+            int root = find(u);
+            totalBeauty += componentBeauty[root];
+        }
+    }
+    cout << totalBeauty << endl;
+    return 0;
+    
 }
