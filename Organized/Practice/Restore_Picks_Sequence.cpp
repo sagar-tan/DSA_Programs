e294eaa7c5888cb143194b947d37d4083d6561a2
sagar-tan <tanwarsagarroboticss2274@gmail.com>
2025-07-11 07:41:40 +0530

    SegmentTreePractice

--- Organized/Practice/Restore_Picks_Sequence.cpp
+++ Organized/Practice/Restore_Picks_Sequence.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int query1(vector<int> &arr,int l,int r){
+void query1(vector<int> &arr,int l,int r){
     int res=0;
-    for(int i = l; i<=r;i++){
+    for(int i = l-1; i<r;i++){
         res+=arr[i];
     }
-    cout << res;
+    cout << res << endl;
 }
 void query2(vector<int> &arr, int l, int r, int x){
-    for(int i = l; i<=r;i++){
+    for(int i = l-1; i<r;i++){
         arr[i]= arr[i] % x;
     }
 
 }
 void query3(vector<int> &arr, int k, int x){
-    arr[k] = x;
+    arr[k-1] = x;
 }
 int main(){
     int n, m;
-    cin << n << m;
-    vector<int> arr;
+    cin >> n >> m;
+    vector<int> arr(n);
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
     int query;
-    while(cin>>query){
+    while(cin >> query){
         if(query==1){
             int l,r;
             cin >> l >> r;
-            query1(arr, l+1, r+1);
+            query1(arr, l, r);
         }
         else if(query==2){
             int l2,r2,x;
             cin >> l2 >> r2 >> x;
-            query2(arr, l2+1, r2+1, x);
+            query2(arr, l2, r2, x);
         }
         else if(query==3){
             int k, x2;
